56703727b37a190c9d26935e793436326c711f54
muXxer <git@muxxer.de>
2022-05-19 22:50:27 +0200

    Add database engine

--- core/participation/params.go
+++ core/participation/params.go
@@ -5,8 +5,13 @@ import (
 )
 
 type ParametersParticipation struct {
-	DatabasePath string `default:"database" usage:"the path to the database folder"`
-	BindAddress  string `default:"localhost:9892" usage:"bind address on which the Participation HTTP server listens"`
+	Database struct {
+		// Engine defines the used database engine (pebble/rocksdb/mapdb).
+		Engine string `default:"rocksdb" usage:"the used database engine (pebble/rocksdb/mapdb)"`
+		// Path defines the path to the database folder.
+		Path string `default:"database" usage:"the path to the database folder"`
+	}
+	BindAddress string `default:"localhost:9892" usage:"bind address on which the Participation HTTP server listens"`
 }
 
 var ParamsParticipation = &ParametersParticipation{}
